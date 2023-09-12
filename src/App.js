611a6faf3aab59121f2125dda4489c42d7612a89
nimrahSohail <102296918+nimrahSohail@users.noreply.github.com>
2023-09-13 01:07:26 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
 
 function App() {
+
+  const [TexttoCopy, setTexttoCopy] = useState('')
+  const [isCopied, setCopied] = useClipboard(TexttoCopy);
+
+  let startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
+  let stopListening = () => SpeechRecognition.stopListening();
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Audio to Written Word Converter</h2>
+      <br />
+      <p>
+        A React hooks module that transforms spoken words captured by the microphone into text and provides accessibility to your React components.
+      </p>
+
+      <div className="main-content" onClick={() => setTexttoCopy(transcript)} >{transcript}</div>
+
+      <div className="btn-style">
+        <button onClick={setCopied}>
+          {isCopied ? "Copied" : "Copy to clipboard"}
+        </button>
+
+        <button onClick={startListening}>Start Listening</button>
+        <button onClick={stopListening}>Stop Listening</button>
+      </div>
     </div>
   );
 }
